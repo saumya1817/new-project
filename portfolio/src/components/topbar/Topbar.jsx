168431ccc859f11927d9a21d4099cf92e08101ba
saumya singh <saumya.singh1817@gmail.com>
2022-07-17 15:35:45 +0530

    setup menu and topbar,excluding material-ui

---
+++ portfolio/src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+// import {Person} from '@material-ui/icons';
+import "./Topbar.scss"
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+
+      <div className="left">
+        <a href="#intro" className="logo">saumya.</a>
+         <div className="itemContainer">
+          {/* <Person className="icon" />  */}
+          <span>+91 123 456 8885</span>
+        </div>
+        
+        <div className="itemContainer">
+          {/* <Mail className="icon"/> */}
+          <span>saumya@gmail.com</span>
+
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+          <span className="line1"></span>
+          <span className="line2"></span>
+          <span className="line3"></span>
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
